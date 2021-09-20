d7a830d8db3261c054fb91f747fa035e5bcc1f4f
Banuchandar <38816671+xeavier@users.noreply.github.com>
2021-09-20 22:55:02 +0530

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -11,9 +11,9 @@ $password = "3HYQvwc7CG";
 $db_name = "WBQV1AlAAP";*/
 
 $sname= "sql10.freesqldatabase.com";
-$unmae= "sql10438653";
-$password = "AEgCMNCg9s";
-$db_name = "user1";
+$unmae= "sql10438681";
+$password = "tM6BKVxar2";
+$db_name = "sql10438681";
 
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 
