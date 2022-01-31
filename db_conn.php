06d944fd22bc5a636333ea9837da2bb285c9689c
Banuchandar <38816671+xeavier@users.noreply.github.com>
2022-01-31 12:06:10 +0530

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -10,10 +10,15 @@ $unmae= "WBQV1AlAAP";
 $password = "3HYQvwc7CG";
 $db_name = "WBQV1AlAAP";*/
 
-$sname= "sql10.freesqldatabase.com";
-$unmae= "sql10438681";
-$password = "tM6BKVxar2";
-$db_name = "sql10438681";
+// $sname= "sql10.freesqldatabase.com";
+// $unmae= "sql10438681";
+// $password = "tM6BKVxar2";
+// $db_name = "sql10438681";
+
+$sname= "sql6.freesqldatabase.com";
+$unmae= "sql6469341";
+$password = "RIzu4QqLLR";
+$db_name = "sql6469341";
 
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 
